d4a7daf710a7d10a165d86ab2743d461bf66e6ef
levmyts <leomyts@gmail.com>
2025-09-28 22:04:09 +0300

    change app's name

--- homebrew-pictoascii/Casks/pictoascii.rb
+++ homebrew-pictoascii/Casks/pictoascii.rb
@@ -3,7 +3,7 @@ cask "pictoascii" do
   sha256 "8c48f7cf84b24e77fc55ad10008636f8627760aa2f9cca905790787da9385185"
 
   url "https://github.com/BR7RKR/picture-to-ascii/releases/download/v1.0.0/pictoascii-macos-arm64.tar.gz"
-  name "pictoascii"
+  name "Pictoascii"
   desc "Small console app that converts picture into ascii symbols image"
   homepage "https://github.com/BR7RKR/picture-to-ascii"
 
